335c5484f68180b667e87442f4d496cf064932ae
Abbi Devins-Suresh <ads@medrhythms.com>
2021-06-17 13:11:17 -0400

    working on adding in React

--- MusicPlayerApp/urls.py
+++ MusicPlayerApp/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 
 urlpatterns = [
     path('Player', include('Player.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('frontend.urls'))
 
 ]
