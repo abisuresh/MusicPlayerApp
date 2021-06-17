0752b1d0f418a887d942edbc40f9f151c99f8603
Abbi Devins-Suresh <ads@medrhythms.com>
2021-06-17 15:57:10 -0400

    working on adding in box component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from "react";
 import {render} from "react-dom"
-import Box from "./Box.js"
+import CustomBox from "./Box.js"
+import Button from '@material-ui/core/Button';
 
-class App extends Component{
-    constructor(props){
+class App extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             data: [],
@@ -12,10 +13,10 @@ class App extends Component{
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         fetch("api/song")
             .then(response => {
-                if(response.status > 400){
+                if (response.status > 400) {
                     return this.setState(() => {
                         return {placeholder: "Error!"};
                     });
@@ -32,25 +33,32 @@ class App extends Component{
             });
     }
 
-    render(){
+    render() {
         return (
-         <ul>
-             {this.state.data.map(song => {
-                 return (
-                     <div style={{backgroundColor: 'green'}}>
-                     <li key={song.id}>
-                         {song.name} - {song.artist}
-                     </li>
-                         
-                    <Box/>
-                     </div>
-                 )
-             })}
-         </ul>
+            <ul>
+                {this.state.data.map(song => {
+                    return (
+                        <div style={{backgroundColor: 'green'}}>
+                            <li key={song.id}>
+                                {song.name} - {song.artist}
+                            </li>
+                            <div className="test">
+                                <CustomBox/>
+
+                                <Button color="primary">
+                                    Exit
+                                </Button>
+                            </div>
+
+
+                        </div>
+                    )
+                })}
+            </ul>
         )
     }
 }
 
 export default App;
 const container = document.getElementById("app");
-render(<App />, container);
+render(<App/>, container);
--- frontend/src/components/Box.js
+++ frontend/src/components/Box.js
@@ -1,15 +1,24 @@
 import React, {Component} from "react";
 import {render} from "react-dom"
 import App from "./App";
+import Box from '@material-ui/core/Box';
 
-class Box extends Component{
-    constructor(props){
+class CustomBox extends Component {
+    constructor(props) {
         super(props);
         this.state = {color: "blue"}
     }
 
-    render(){
-        return <div>{this.state.color}</div>
+    render() {
+        return <div><Box sx={{
+            width: 300, height: 300, bgcolor: 'primary.dark', '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.8, 0.9, 0.8],
+            },
+        }}
+        />
+        </div>
     }
 }
-export default Box;
+
+export default CustomBox;
